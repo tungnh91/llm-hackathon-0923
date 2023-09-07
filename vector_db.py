f430fe30b9893a4136f904ca08596b774cc37796
Tim Nguyen <timnguyen@m2mx.attlocal.net>
2023-09-07 15:12:50 -0700

    add weaviate

--- vector_db.py
+++ vector_db.py
@@ -1,2 +1,11 @@
+import os
+import openai
+import weaviate
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+client = weaviate.Client("http://localhost:8080")
+
+
 def put_in_db(file):
     print("done")
